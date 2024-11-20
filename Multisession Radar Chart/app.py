bc616174d0b0d83361e484c5878bc07654917efa
GashDarnit <edisontanh@gmail.com>
2024-11-20 18:18:58 +0800

    - Implement user selection drop-down menu - Implemented Session selection menu based on selected user - Implemented Styling into page via CDN Tailwind CSS - Implemented Radar Chart Generation using amChart V4 - Implemented Dynamic Radar Chart Generation based on user requirements - Implemented Radar Chart Generation using Data received from backend - Implemented Multiple plots for multiple sessions for Radar Chart - Implemented Remarks Feature - Add Remarks, Edit Remarks, Delete Remarks - Implemented Report Generation based on user requirements - Implemented Segmented LaTeX report generation via multiple LaTeX templates - Implemented Download Report feature

---
+++ Multisession Radar Chart/app.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify, send_file
+import os
+import subprocess
+import jinja2
+import json
+
+app = Flask(__name__)
+
+# Paths
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+REPORTS_DIR = os.path.join(STATIC_DIR, 'reports')
+
+# Jinja2 Environment for LaTeX templates
+latex_jinja_env = jinja2.Environment(
+    block_start_string='\BLOCK{',
+    block_end_string='}',
+    variable_start_string='\VAR{',
+    variable_end_string='}',
+    comment_start_string='\#{',
+    comment_end_string='}',
+    line_statement_prefix='%%',
+    line_comment_prefix='%#',
+    trim_blocks=True,
+    autoescape=False,
+    loader=jinja2.FileSystemLoader(os.path.join(BASE_DIR, 'static', 'template'))
+)
+
+################ Helper Methods #########################
+def generate_report(data, dates, colors, name, remarks):
+    # Paths for the TeX and PDF files
+    output_tex_path = os.path.join(REPORTS_DIR, name + '.tex')  # Path for the .tex file
+    output_pdf_path = os.path.join(BASE_DIR, name + '.pdf')  # Path for the .pdf file
+    logo_path = os.path.join(STATIC_DIR, 'images', 'logo_tight.png')
+    logo_path = logo_path.replace("\\", "/")
+
+    # Ensure the reports directory exists
+    if not os.path.exists(REPORTS_DIR):
+        os.makedirs(REPORTS_DIR, exist_ok=True)
+
+    # Generate the report content using the template
+    template = latex_jinja_env.get_template('WebChart.tex')
+    template_remarks = latex_jinja_env.get_template('remarks.tex')
+    template_end = latex_jinja_env.get_template('end.tex')
+    output = template.render(
+        logo_icon=logo_path,
+        athlete_name=name,
+        data=data,  # performance data (consistency, grouping, etc.), joints
+        dates=dates,  # dates/sessions
+        colors=colors,  # Array of hard-coded colors
+    )
+    
+    if(len(remarks) > 0):
+        output += template_remarks.render(
+            remarks=remarks
+        )
+    
+    output += template_end.render()
+
+    # Write the .tex file
+    with open(output_tex_path, 'w') as f:
+        f.write(output)
+    
+    print(f"TeX file written: {output_tex_path}")
+    
+    # Run pdflatex to generate the PDF
+    escaped_tex_path = f'"{output_tex_path}"'  # Escape path for spaces
+    os.system(f"pdflatex -interaction nonstopmode {escaped_tex_path}")
+    
+    # Check if the PDF was created
+    if os.path.exists(output_pdf_path):
+        print(f"PDF successfully created: {output_pdf_path}")
+        return output_pdf_path
+    else:
+        print(f"Failed to create PDF: {output_pdf_path}")
+        return os.path.join(BASE_DIR, 'templates', 'error.html')
+
+
+
+def read_data():
+    with open('data.json') as f:
+        data = json.load(f)
+    return data
+
+
+############### Routes #####################
+@app.route('/')
+def index():
+    print(REPORTS_DIR)
+    
+    data = read_data()
+    users = data.keys()
+    return render_template('index.html', users=users)
+
+@app.route('/get-user-data/<username>')
+def get_user_data(username):
+    data = read_data()
+    user_data = data.get(username, {})
+
+    if not user_data:
+        return "<p>No data available for this user.</p>", 404
+
+    # Generate the user data in JSON format to use for chart rendering
+    return jsonify(user_data)
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    try:
+        # Fetching data from the request
+        content = request.get_json()
+        data = content['data']
+        name = content['name']
+        remarks = content['remarks']
+        dates = list(data.keys())
+        colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'brown',  'gray', 'violet', 'olive']
+        
+        if not os.path.exists(REPORTS_DIR):
+            os.makedirs(REPORTS_DIR, exist_ok=True)
+
+        output_path = generate_report(data, dates, colors, name, remarks)
+        return send_file(output_path, as_attachment=True, download_name=name + '.pdf')
+
+    except Exception as e:
+        print("Error processing the data:", e)
+        return jsonify({"error": "Error generating report"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Multisession Radar Chart/static/js/main.js
@@ -0,0 +1,169 @@
+// Keep global variable to avoid constantly doing GET requests to the backend
+var currentData = {};
+var selectedUser = undefined;
+var addedRemarks = [];
+var deleteIndex = undefined;
+
+function resetInfo(newUser, newData) {
+    currentData = newData;
+    selectedUser = newUser;
+    addedRemarks = [];
+    deleteIndex = undefined;
+}
+
+
+function updateRemarksList() {
+    const remarksListContainer = $('#remarks-items');
+    remarksListContainer.empty(); // Clear the list first
+
+    // Add each remark as a card
+    addedRemarks.forEach((remark, index) => {
+        const remarkCard = $('<div>')
+            .addClass('bg-gray-100 p-4 rounded-lg shadow-md mb-2 cursor-pointer')
+            .append(
+                $('<p>').text(remark).addClass('text-gray-800')
+            )
+            .click(function() {
+                // Clear the previously selected card
+                $('#remarks-items .selected').removeClass('selected');
+
+                // Mark this card as selected
+                $(this).addClass('selected');
+
+                // When a remark card is clicked, populate the textarea for editing
+                $('#remarks-text').val(remark);
+
+                // Store the index of the remark being edited in the textarea
+                $('#remarks-text').data('edit-index', index);
+            });
+
+        // Append the remark card to the list
+        remarksListContainer.append(remarkCard);
+    });
+}
+
+function deleteRemark(index) {
+    addedRemarks.splice(index, 1); // Remove the remark from the array
+    updateRemarksList(); // Update the remarks list to reflect the deletion
+    deleteIndex = undefined;
+}
+
+function getScrollbarWidth() {
+    // Create a temporary div element with scrollbars
+    const div = document.createElement('div');
+    div.style.overflow = 'scroll';
+    div.style.visibility = 'hidden';
+    div.style.position = 'absolute';
+    div.style.width = '100px';
+    div.style.height = '100px';
+    document.body.appendChild(div);
+    
+    const scrollbarWidth = div.offsetWidth - div.clientWidth;
+    
+    // Remove the temporary div element
+    document.body.removeChild(div);
+    
+    return scrollbarWidth;
+}
+
+// Function to retrieve selected sessions
+function getSelectedSessions() {
+    var selectedSessions = [];
+    // Loop through all the session cards that are marked as selected
+    $('#sessions-form .bg-gray-300').each(function() {
+        selectedSessions.push($(this).attr('data-session')); // Get the session key
+    });
+    return selectedSessions;
+}
+
+
+function generateRadarChart(originalJSON) {
+    // Clear the previous chart if it exists
+    $('#performanceChart').empty();
+    $('#jointsChart').empty();
+    
+    var colors = ['red', 'lime', 'blue', 'orange', 'magenta', 'purple', 'brown', 'gray', 'violet', 'cyan'];
+
+    // Transform the data dynamically
+    var performanceData = Object.keys(originalJSON).map((dateKey, index) => {
+        var entry = originalJSON[dateKey];
+        return {
+            name: dateKey, // Use the date as the dataset name
+            data: [
+                { "performance": "Grouping", "score": entry.grouping },
+                { "performance": "Consistency", "score": entry.consistency },
+                { "performance": "DFL", "score": entry.dfl },
+                { "performance": "Anchor", "score": entry.anchor },
+                { "performance": "Bow Stability", "score": entry["bow stability"] },
+                { "performance": "Bow Balance", "score": entry["bow balance"] },
+                { "performance": "Strength", "score": entry.strength },
+                { "performance": "Aiming", "score": entry.aiming }
+            ]
+        };
+    });
+
+    // Create a dataset for joints
+    var jointsData = Object.keys(originalJSON).map((dateKey) => {
+        var entry = originalJSON[dateKey];
+        return {
+            name: dateKey, // Use the date as the dataset name
+            data: entry.joints.map((value, index) => ({
+                performance: `J${String(index + 1).padStart(2, "0")}`, // J01, J02, ...
+                score: value
+            }))
+        };
+    });
+    
+    am4core.ready(function () {
+        am4core.useTheme(am4themes_animated);
+
+        function createRadarChart(divId, chartData) {
+            var chart = am4core.create(divId, am4charts.RadarChart);
+            chart.data = chartData[0].data;
+
+            chart.maskBullets = false;
+            chart.fontFamily = "Arial";
+
+            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+            categoryAxis.dataFields.category = "performance";
+            categoryAxis.renderer.tooltipLocation = 0.01;
+
+            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            valueAxis.renderer.axisFills.template.fillOpacity = 0.05;
+            valueAxis.renderer.gridType = "polygons";
+            
+            if(divId === "performanceChart") valueAxis.min = 40;
+            else valueAxis.min = 70;
+            valueAxis.max = 100;
+            valueAxis.renderer.labels.template.opacity = 0.5;
+            valueAxis.strictMinMax = true;
+
+            chartData.forEach((dataset, index) => {
+                var series = chart.series.push(new am4charts.RadarSeries());
+                series.data = dataset.data;
+                series.dataFields.valueY = "score";
+                series.dataFields.categoryX = "performance";
+                series.name = dataset.name;
+                series.strokeWidth = 3;
+
+                var color = colors[index];
+                series.stroke = am4core.color(color);
+                series.tooltip.getFillFromObject = false;
+                series.tooltip.getStrokeFromObject = true;
+                series.tooltip.background.fill = am4core.color(color);
+                series.tooltip.background.cornerRadius = 5;
+
+                var bullet = series.bullets.push(new am4charts.CircleBullet());
+                bullet.circle.fill = am4core.color("#F2A78F");
+                bullet.circle.stroke = am4core.color(color);
+                bullet.circle.strokeWidth = 2;
+                bullet.tooltipText = "{valueY}";
+            });
+
+            chart.legend = new am4charts.Legend();
+        }
+
+        createRadarChart("performanceChart", performanceData);
+        createRadarChart("jointsChart", jointsData);
+    });
+}
